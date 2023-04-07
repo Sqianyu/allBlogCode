bc6f2cdcc36f79bdbc2c36fd07f6e69b0cdc5c62
sunqy <sunqy@fxiaoek.com>
2023-04-07 11:19:54 +0800

    init

---
+++ src/components/header/navigation.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="home-navigation">
+        <div class="navbar">22</div>
+        <div>22</div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+
+  }
+  </script>
+  
+  <style lang="less" scoped>
+  .home-navigation {
+    width: 100%;
+    height: 56px;
+    line-height: 56px;
+    background-color: #fff;
+    display: flex;
+    .navbar {
+        width: 400px;
+        margin-left: 100px;
+    }
+  }
+  </style>
