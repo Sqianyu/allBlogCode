01722725170541892d1d72defe9fbdcd73999676
sunqy <sunqy@fxiaoek.com>
2024-05-17 10:25:18 +0800

    update

--- src/store/index.js
+++ src/store/index.js
@@ -1,17 +1,25 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import persistedstate from 'vuex-persistedstate'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    user: {}
   },
   getters: {
   },
   mutations: {
+    setUsername(state,user) {
+      state.user = user
+    }
   },
   actions: {
   },
   modules: {
-  }
+  },
+  plugins:[
+    persistedstate() 
+  ]
 })
--- src/views/user/personal.vue
+++ src/views/user/personal.vue
@@ -1,13 +1,39 @@
 <template>
-  <div>个人中心</div>
+  <div>
+    <img :src="imgPath" alt="" class="small-img">
+    <el-upload
+      action="http://localhost:3000/upload/img"
+      name="myfile"
+      :headers="uploadHeader"
+      :show-file-list="false"
+      :on-success="onSuccess">
+      <el-button>上传</el-button>
+    </el-upload>
+  </div>
 </template>
 
 <script>
 export default {
-
+  data(){
+    return {
+      uploadHeader: {
+        Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VybmFtZSI6ImFkbWluIiwiX2lkIjoiNjYzYTE4ZWUwY2VlNDQzZTA4NGE4ZjQ5IiwiaWF0IjoxNzE1NjU0NDk2LCJleHAiOjE3MTYyNTkyOTZ9.ZRr8txmkls6y_vedFYpTX9Xosussps9pBJfZMgS3zjw"
+      },
+      imgPath: ''
+    }
+  },
+  methods: {
+    onSuccess(res) {
+      this.imgPath = 'http://localhost:3000/' + res.data.path.split('public/')[1]
+    }
+  }
 }
 </script>
 
-<style>
-
+<style scoped>
+.small-img {
+  width: 100px;
+  height: 100px;
+  margin-right: 20px;
+}
 </style>
